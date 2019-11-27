0aaa9e2c641a92ad5eac81e34f004a08893c6206
MAN\BDUDERSTAD <brad.duderstadt@coxautoinc.com>
2019-11-27 12:29:03 -0600

    Replace all instances of next-starter with next-sample

--- .next/static/runtime/main.js
+++ .next/static/runtime/main.js
@@ -5139,7 +5139,7 @@ function connect(options) {
       error.name = err.name;
       error.stack = err.stack; // __NEXT_DIST_DIR is provided by webpack
 
-      (0, _sourceMapSupport.rewriteStacktrace)(error, "D:\\Development\\React\\next-starter\\.next");
+      (0, _sourceMapSupport.rewriteStacktrace)(error, "D:\\Development\\React\\next-sample\\.next");
       return error;
     }
   };
